8f8ffbbb2db899f0f877309fd5872d88e58b9b13
danikarasawa <danikarasawa@gmail.com>
2019-09-30 21:58:23 -0300

    new changes2

--- ex1.js
+++ ex1.js
@@ -76,21 +76,20 @@
 // 9- dada a array const lista = [56, 89, 34, 12, 23, 55, 9]
 // ordene a lista .
 
-const lista = [56, 89, 34, 12, 23, 55, 09];
+//const lista = [56, 89, 34, 12, 23, 55, 09];
 // lista.sort(function(a, b){return a-b});
 // console.log(lista);
 //lista.sort((a,b)=> a-b) arrow function
 
-for (let i = 0; i < lista.length; i++){
-    for(let j = 0; j < lista.length; j++){
-        if(lista[i] > lista[i+1]){
-            const aux = lista[i]
-            lista[i] = lista[i+1]
-            
-        }
-    }
-};
-
+// for (let i = 0; i < lista.length; i++) {
+//     for (let j = 0; j < lista.length; j++) {
+//         if (lista[j] > lista[j + 1]) {
+//             const aux = lista[j]
+//             lista[j] = lista[j + 1]
+//             lista[j + 1] = aux
+//         }
+//     }
+// };
 
 // LOOP NA MÃO PRA ENTENDER O PROCESSO 
 
